fd46696de7999aa3689d338c4275ab0ff6b1aa46
lannur-s <lathaannur@gmail.com>
2023-05-03 18:00:07 +0200

    first setup

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 12:46:47 by lannur-s          #+#    #+#             */
-/*   Updated: 2023/05/03 16:27:27 by lannur-s         ###   ########.fr       */
+/*   Updated: 2023/05/03 16:55:14 by lannur-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@
 # include <stdio.h>
 # include <ctype.h>
 
-int		ft_isalpha(int c);
+int			ft_isalpha(int c);
 
-int		ft_isdigit(int c);
+int			ft_isdigit(int c);
 
-int		ft_isascii(int c);
+int			ft_isascii(int c);
 
-int		ft_isprint(int c);
+int			ft_isprint(int c);
 
-int		ft_isalnum(int c);
+int			ft_isalnum(int c);
 
-int		ft_tolower(int c);
+int			ft_tolower(int c);
 
-int		ft_toupper(int c);
+int			ft_toupper(int c);
 
 #endif
