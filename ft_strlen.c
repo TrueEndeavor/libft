1807184dc2365e2d9c3ebf553603cfc7bd246a63
lannur-s <lathaannur@gmail.com>
2023-05-03 18:25:12 +0200

    set 2 - first try

---
+++ ft_strlen.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 18:12:22 by lannur-s          #+#    #+#             */
+/*   Updated: 2023/05/03 18:23:47 by lannur-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	unsigned int		i = 0;
+	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char arr[10] = "helloworld";
+	printf("%ld", ft_strlen(arr));
+
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,28 +6,36 @@
 /*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 12:46:47 by lannur-s          #+#    #+#             */
-/*   Updated: 2023/05/03 16:55:14 by lannur-s         ###   ########.fr       */
+/*   Updated: 2023/05/03 18:15:32 by lannur-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stdio.h>
-# include <ctype.h>
+# include <stdlib.h>
+# include <string.h>
 
 int			ft_isalpha(int c);
-
 int			ft_isdigit(int c);
-
 int			ft_isascii(int c);
-
 int			ft_isprint(int c);
-
 int			ft_isalnum(int c);
-
 int			ft_tolower(int c);
-
 int			ft_toupper(int c);
+
+size_t		ft_strlen(const char *s);
+
+void		ft_bzero(void *s, size_t n);
+
+void		*ft_memset(void *s, int c, size_t n);
+
+void		*ft_memmove(void *dest, const void *src, size_t n);
+
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+
+void		*ft_memchr(const void *s, int c, size_t n);
+
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
