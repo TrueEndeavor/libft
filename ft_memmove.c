0f7411088b524654b8d315adc83103bc47e6f054
lannur-s <lathaannur@gmail.com>
2023-05-16 18:07:15 +0200

    round-1 verification

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lannur-s <lannur-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 18:12:13 by lannur-s          #+#    #+#             */
-/*   Updated: 2023/05/16 15:37:04 by lannur-s         ###   ########.fr       */
+/*   Updated: 2023/05/16 18:04:16 by lannur-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char		*xd;
-	const char	*xs;
+	unsigned char		*xd;
+	const unsigned char	*xs;
 
+	if (dest == src)
+		return (dest);
 	xd = dest;
 	xs = src;
-	if (xd == xs)
-		return (dest);
-	if (xd < xs)
-		memcpy(xd, xs, n);
-	else
+	if (src < dest && dest - src < (long) n)
 		while (n--)
-			xd[n] = xs[n];
+			*(xd + n) = *(xs + n);
+	else
+		memcpy(xd, src, n);
 	return (dest);
 }
